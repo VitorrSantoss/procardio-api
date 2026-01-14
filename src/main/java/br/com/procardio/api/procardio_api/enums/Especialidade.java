510d6e718df418d3c678341e7f6602d8915e1f11
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-01-14 15:13:51 -0300

    entidades adicionadas

---
+++ src/main/java/br/com/procardio/api/procardio_api/enums/Especialidade.java
@@ -0,0 +1,10 @@
+package br.com.procardio.api.procardio_api.enums;
+
+// Enum para especialidades médicas
+public enum Especialidade {
+    CARDIOLOGISTA,
+    DERMATOLOGISTA,
+    PEDIATRIA,
+    GINECOLOGISTA,
+    ORTOPEDIA
+}
