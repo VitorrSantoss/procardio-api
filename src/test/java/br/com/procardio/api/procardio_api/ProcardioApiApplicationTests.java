2a041bedfb159878757e984ab1e2699b84f4e32b
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-01-14 15:24:37 -0300

    test: adicionar teste de carregamento de contexto

---
+++ src/test/java/br/com/procardio/api/procardio_api/ProcardioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.procardio.api.procardio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcardioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
