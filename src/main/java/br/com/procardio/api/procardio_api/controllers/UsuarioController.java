d8042c5bfcd7bbe41f4edfd8f898b02f2bec7405
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-01-16 15:11:30 -0300

    feat: atualizar controller de usuário

--- src/main/java/br/com/procardio/api/procardio_api/controllers/UsuarioController.java
+++ src/main/java/br/com/procardio/api/procardio_api/controllers/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,15 +25,19 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     // Endpoint para cadastrar um novo usuário
+    @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        // Chama o serviço para salvar o novo usuário
         Usuario novoUsuario = usuarioService.salvarUsuario(usuarioDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
     }
 
     // Endpoint para atualizar um usuário existente
+    @PutMapping("/{id}")
     public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioDTO usuarioDTO) {
+        // Chama o serviço para atualizar o usuário
         Usuario usuarioAtualizado = usuarioService.salvarUsuario(id, usuarioDTO);
-        
+
         // Verifica se o usuário foi encontrado e atualizado
         if (Objects.nonNull(usuarioAtualizado)) {
             return ResponseEntity.ok(usuarioAtualizado);
