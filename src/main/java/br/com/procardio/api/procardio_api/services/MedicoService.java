f4d7f5d7f41a8dc69007c40f90807c38c50218da
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-01-16 15:13:42 -0300

    feat: atualizar serviço de médico com comentários

--- src/main/java/br/com/procardio/api/procardio_api/services/MedicoService.java
+++ src/main/java/br/com/procardio/api/procardio_api/services/MedicoService.java
@@ -15,30 +15,37 @@ public class MedicoService {
     @Autowired
     private MedicoRepository medicoRepository;
 
+    // BUSCAR MÉDICO POR ID
     public Medico buscarPorId(Long id) {
         return medicoRepository.findById(id).orElse(null);
     }
 
+    // BUSCAR MÉDICO POR EMAIL
     public Medico buscarMedicoPorEmail(String email) {
         return medicoRepository.findByEmail(email).orElse(null);
     }
 
+    // BUSCAR MÉDICO POR CRM
     public Medico buscarMedicoPorCrm(String crm) {
         return medicoRepository.findByCrm(crm).orElse(null);
     }
 
+    // BUSCAR MÉDICOS POR ESPECIALIDADE
     public List<Medico> buscarMedicosPorEspecialidade(Especialidade especialidade) {
         return medicoRepository.findByEspecialidade(especialidade);
     }
 
+    // SALVAR MÉDICO
     public Medico salvarMedico(Medico medico) {
         return medicoRepository.save(medico);
     }
 
+    // DELETAR MÉDICO
     public void deletarMedico(Long id) {
         medicoRepository.deleteById(id);
     }
 
+    // LISTAR TODOS OS MÉDICOS
     public List<Medico> listarTodosMedicos() {
         return medicoRepository.findAll();
     }
