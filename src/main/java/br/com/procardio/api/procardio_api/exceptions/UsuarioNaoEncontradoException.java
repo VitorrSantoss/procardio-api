cbd493926b352bbbf84657a1904e195d0b3aeeca
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-01-16 15:12:16 -0300

    fix: ajustar exceção de email não encontrado

--- src/main/java/br/com/procardio/api/procardio_api/exceptions/UsuarioNaoEncontradoException.java
+++ src/main/java/br/com/procardio/api/procardio_api/exceptions/UsuarioNaoEncontradoException.java
@@ -7,4 +7,9 @@ public class UsuarioNaoEncontradoException extends RuntimeException {
     public UsuarioNaoEncontradoException(Long id) {
         super("Usuário com ID " + id + " não encontrado.");
     }
+
+    // Construtor que recebe o email do usuário não encontrado
+    public UsuarioNaoEncontradoException(String email) {
+        super("Usuario com email " + email + " não encontrado.");
+    }
 }
